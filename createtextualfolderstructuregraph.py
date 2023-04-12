791b4a6fb077052d1ff4e2aa05d91d1f6b4adefe
Swamy Ghanta <swamy.ghanta@nshift.com>
2023-04-12 12:11:06 +0200

    working version for expected tree graph

--- createtextualfolderstructuregraph.py
+++ createtextualfolderstructuregraph.py
@@ -1,6 +1,6 @@
 import os
 
-def create_tree_structure(folder_path, output_path, prefix=''):
+def create_tree_structure(folder_path, output_path=None, prefix=''):
     """
     Recursively creates a textual tree structure for all files and folders within a given folder path.
     """
@@ -11,22 +11,29 @@ def create_tree_structure(folder_path, output_path, prefix=''):
     items = os.listdir(folder_path)
 
     # iterate over the list of items
-    with open(output_path, 'a') as f:
-        for index, item in enumerate(sorted(items)):
-            # get the full path of the item
-            item_path = os.path.join(folder_path, item)
+    for index, item in enumerate(sorted(items)):
+        # get the full path of the item
+        item_path = os.path.join(folder_path, item)
 
-            # check if the item is a file or a folder
-            if os.path.isfile(item_path):
-                # write the file name with a prefix indicating its position in the tree structure
-                f.write(f"{prefix}├── {item}\n")
-            elif os.path.isdir(item_path):
-                # write the folder name with a prefix indicating its position in the tree structure
-                f.write(f"{prefix}├── {item}/\n")
-                # recursively call the function to create the tree structure for the subfolder
-                if index == len(items)-1:
-                    create_tree_structure(item_path, output_path, prefix + "    ")
-                else:
-                    create_tree_structure(item_path, output_path, prefix + "│   ")
+        # check if the item is a file or a folder
+        if os.path.isfile(item_path):
+            # write the file name with a prefix indicating its position in the tree structure
+            if output_path is None:
+                print(f"{prefix}├── {item}")
+            else:
+                with open(output_path, 'a') as f:
+                    f.write(f"{prefix}├── {item}\n")
+        elif os.path.isdir(item_path):
+            # write the folder name with a prefix indicating its position in the tree structure
+            if output_path is None:
+                print(f"{prefix}├── {item}/")
+            else:
+                with open(output_path, 'a') as f:
+                    f.write(f"{prefix}├── {item}/\n")
 
+            # recursively call the function to create the tree structure for the subfolder
+            if index == len(items)-1:
+                create_tree_structure(item_path, output_path, prefix + "    ")
+            else:
+                create_tree_structure(item_path, output_path, prefix + "│   ")
 
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from createfilesandfolders import *
 from createtextualfolderstructuregraph import *
 
-
+'''
 timestamp = time.strftime("%Y%m%d-%H%M%S")
 folder_path = f"/home/swamyg/Workspaces/Tests/Python-{timestamp}"
-depth = 3  # n level depth
-breadth = 2  # m level breadth
-max_depth = 3  # maximum depth
+depth = 2  # n level depth
+breadth = 1  # m level breadth
+max_depth = 2  # maximum depth
 os.makedirs(folder_path)
 create_folders(folder_path, 0, breadth, max_depth)
+'''
 
 
 folder_path = "/home/swamyg/repos/saltstack"
 output_path = "/home/swamyg/Workspaces/Tests/saltstack.txt"
+# folder_path = "/home/swamyg/Workspaces/Tests"
+# output_path = "/home/swamyg/Workspaces/Tests/output.txt"
 with open(output_path, 'w') as f:
     f.write(f"{folder_path}/\n")
 create_tree_structure(folder_path, output_path)
